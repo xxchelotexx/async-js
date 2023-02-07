e96a4ffab3d0bcc197465ccccb2c03e37a678470
Marcelo Ruiz <ruizv.marcelo@gmail.com>
2023-02-07 06:20:36 -0400

    Almost ready

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,7 @@
 const API = 'https://youtube-v31.p.rapidapi.com/captions?part=snippet&videoId=M7FIvfx5J10';
+
+const content = null || document.getElementById('content');
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,9 +19,26 @@ async function fetchData (urlAPI){
 (async()=>{
     try{
         const videos = await fetchData(API);
-        let view = ``;
-    }catch{
-
+        let view = `
+        ${videos.items.map(video=>`
+            <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+                title
+                </h3>
+            </div>
+        ` ).slice(0,4).join('')}
+            
+        `;
+        content.innerHTML = view;
+    }catch(error){
+        console.log(error);
     }
     
 })();
